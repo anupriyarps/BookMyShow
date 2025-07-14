dec6e87acc6f6ef22cfd7cafbe37ae8bc19123a3
anupriyarps <anupriyarps@example.com>
2025-07-15 03:07:01 +0530

    SeatLayout Added

---
+++ src/lib/dateFormate.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const dateFormate = (date) => {
+  return new Date(date).toLocaleDateString('en-US', {
+    weekday: 'short',
+    month: 'long', 
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+    })
+}
+
+export default dateFormate
